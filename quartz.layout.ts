406eea75080b91c1eee9d3db22d88b62227b6f17
Pelayo Arbues <pgarbues@idealista.com>
2025-04-15 15:21:11 +0200

    vault backup: 2025-04-15 15:21:11

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -18,6 +18,11 @@ export const sharedPageComponents: SharedLayout = {
       category: 'Announcements',
       // from data-category-id
       categoryId: 'DIC_kwDOIu8cE84CpHjV',
+      themeUrl: "https://pelayoarbues.github.io/pelayoarbues/static/giscus", // corresponds to quartz/static/giscus/
+      lightTheme: "light", // corresponds to light-theme.css in quartz/static/giscus/
+      darkTheme: "dark", // corresponds to dark-theme.css quartz/static/giscus/
+      mapping: "pathname",
+      strict: false,
     }
   }),
    ],
